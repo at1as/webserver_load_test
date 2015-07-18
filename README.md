a45c0329f06a80a8127f13df8fd8f71249a363e4
Jason Willems <jason@willems.ca>
2015-07-18 18:29:54 -0400

    Many improvements to headless tests, waiting for ajax elements, etc

--- README.md
+++ README.md
@@ -1,30 +1,83 @@
 # web_load_test
 
-Flexible load test utilities for testing your web application.
+Flexible load test utilities for testing your web application. Aims to generalize some aspects of load testing, so that separate applications can use the same script with minimal effort put into reinventing the generic script setup and teardown procedures.
 
 Load testing is distributed across two seperate tools, `spawn_browsers.py` and `spawn_api_requests.py`. Configuration files are stored in the config/ folder
 
 
 ```bash
-config/
-	authentication.json
-	url_list.json
-	browser_totals.json
-runner/
-	selenium.py
-	api_calls.py
+├── config
+│   ├── authentication.json
+│   ├── browser_totals.json
+│   └── url_list.json
+└── runner
+    ├── spawn_api_requests.py
+    └── spawn_browsers.py
 ```
 
-### Configuration Files
+### Installation
 
+`$ git clone https://github.com/at1as/web_load_test.git`
+`npm install -g phantomjs`
+`pip install selenium`
+
+### Configuration Files
+ 
 #### authentication.json
+
+Note, currently only supports payload authentication.
+
+"browser_url_endpoint", "username", "password" and "submit_buttom" are for selenium testing in spawn_browsers.py. 
+"api_auth_endpoint" and "api_auth_payload" are for spawn_requests.py.
+
+* browser_authentication
+* 	enabled => Whether to authenticate before traversing urls [true or false]
+* 	endpoint => Url endpoint of the browser login page [ex "login"]
+* username
+*   enabled => Whether to supply a username [true or false]
+*   value => The username string [ex "user1"]
+*   dom_value => The value of the dom key to search for to identify the username field [ex. "username_input"] 
+*   dom_element_type => The type of element attribute to search for to identify username field ["id" or "name"]
+* password
+*   enabled => Whether to supply a password [true or false]
+*   value => The password string [ex "password1"]
+*   dom_value => the value of the element key to search for to identify the password field [ex. "password_input"]
+*   dom_element_type => the type of element attribute to search for to identify the password field ["id" or "name"]
+* submit_button
+*   dom_value => the value of the element key to search for to identify [ex. "submit_btn"]
+*   dom_element_type => the type of element attribute to search for the identify the submit button ["id" or "name"]
+* api_authentication
+*   enabled => Whether to enable API authentication [true or false]
+*   endpoint => The endpoint of the authentication API call (which is not necessary the same as the browser url) [ex. "/authenticate"]
+*   request_type => The type of API call needed to authenticate [typically "post", sometimes "put"]
+*   payload_as_string => the body the api request must send to authenticate [ex. "{'username':'user1', 'password':'pass1'}"]
+*   payload_format => the format of the body ["application/json", "application/xml", "text/plain"]
+ 
 #### url_list.json
+   
+* protocol => The protocol to use ["http" or "https"]
+* url => base url [ex. "www.example.com"]
+* endpoints => list of url endpoints to search, [ex. "login", "logout", "stats", "/"]
+
 #### browser_totals.json
+   
+* Modify the value beside each browser name (as an integer) to match the desired browser breakdown [chrome, headless (phantomJS) and firefox]
+
+```bash
+{
+  "headless": 1,
+  "firefox": 1,
+  "chrome": 1
+}
+```
+Above, all browsers will run simultaneously. Enter for quantity to disable one browser. Ensure the selenium webdriver is installed for each which you intend to use, and note that the only supported headless browser right now is PhantomJS (tested with version 1.9.8)
 
 ### Test Runners
 
+Run all scripts from the root directory (i.e., `python runner/spawn_browsers.py`)
+   
 #### Spawn_Browsers.py
-
+ 
 This script will use the selenium web driver to spawn desired browsers and visit pages specified in url_list.json. Can spawn both headless browsers, like PhantomJS, and real browsers like Firefox and Chrome.  
 
 Increasing the number of headless phantomjs browsers will allow the computer running this script to spawn more browser sessions (than with firefox, chrome or other browsers), and will ensure all dynamic AJAX content is loaded (dynamic DOM elements, on page load API requests, etc). The server you are testing against should experience roughly the same load with phantomjs as it would with, for instance, a firefox client (as measured by the database and web server)
@@ -40,12 +93,12 @@ However, headless browsers have some limitations:
 * Phantomjs has other non-python and non-javascript system dependencies, and some are explicitly required to be 32 bit. Refer to the generated ghostdriver.log for missing dependencies.
 * Especially if running on a Mac, but also on Linux, with you may run into a limit on files open on the server. These limits are configurable in your OS
 
-
+ 
 #### Spawn_API_Requests.py
-
+ 
 Will issue REST requests using the Python Requests library. This should allow your computer to spawn many more simultaneous requests (perhaps an order of magnitude more than spawn_browsers.py using real browsers). However, for HTML webpages, dynamic elements will not be loaded, which may result in much lower load to the database and web server (if, for instance, every page dynamically loads its images, or portions of the DOM, which would result in even more requests).
 
 ##### Limitations
-
+ 
 * Especially if running on a Mac, but also on Linux, with many concurrent threads you may run into a limit on threads or processes on number of files open on the server. These limits are configurable in your OS
 * Requests has strick SSL policies. All verification has been disabled in the script
--- runner/spawn_browsers.py
+++ runner/spawn_browsers.py
@@ -13,21 +13,54 @@ import json
 # Developed with Python 2.7.6, PhantomJS 1.9.8
 # Tested on OS X and Linux
 
+def find_element_and_fill(browser, selector, value, field_value):
+  # Find element by selector (name or id) and value, and send field_value string to element
+
+  if selector == "name":
+    WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.NAME, value)))
+    browser.find_element_by_name(value).send_keys(field_value)
+  elif selector == "id":
+    WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.ID, value)))
+    browser.find_element_by_id(value).send_keys(field_value)
+  else:
+    raise "Invalid Selector type for %s" %value
+
+def find_element_and_click(browser, selector, value):
+  # Find element by selector (name or id) and value, and click
+
+  if selector == "name":
+    WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.NAME, value)))
+    browser.find_element_by_name(value).click()
+  elif selector == "id":
+    WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.ID, value)))
+    browser.find_element_by_id(value).click()
+  else:
+    raise "Invalid Selector type for login buttom %s" %value
+
 
 # Browser Actions
 def authenticate(instance):
-  print("Browser %s : Authenticating as %s..." %((instance+1), user['name']))
+  print("Browser %s : Authenticating as %s..." %((instance+1), authentication['username']['field_value']))
   
-  login_page = "%s://%s/%s" %(url_details['protocol'], url_details['url'], authentication['browser_url_endpoint'])
-  browser_instances[instance].get(pages[login_page])
-  
-  #wait = WebDriverWait(browser_instances[instance], 10)
-  #wait.until(EC.invisibility_of_element_located((By.NAME, 'username')))
-  #print browser_instances[instance].page_source.encode("utf-8")
+  login_page = "%s://%s/%s" %(url_details['protocol'], url_details['url'], authentication['browser_authentication']['endpoint'])
+  browser_instances[instance].get(login_page)
 
-  browser_instances[instance].find_element_by_name('username').send_keys(user['name'])
-  browser_instances[instance].find_element_by_id('password').send_keys(user['password'])
-  browser_instances[instance].find_element_by_id("login").click()
+  if authentication['username']['enabled']:
+    find_element_and_fill(browser_instances[instance],
+                          authentication['username']['dom_element_type'],
+                          authentication['username']['dom_value'],
+                          authentication['username']['field_value'])
+
+  if authentication['password']['enabled']:
+    find_element_and_fill(browser_instances[instance],
+                          authentication['password']['dom_element_type'],
+                          authentication['password']['dom_value'],
+                          authentication['password']['field_value'])
+
+  find_element_and_click( browser_instances[instance],
+                          authentication['submit_button']['dom_element_type'],
+                          authentication['submit_button']['dom_value'])
+
 
 def go_to_page(instance, page):
   print("Browser %s : Navigating to %s..." %(instance+1, page))
@@ -38,17 +71,19 @@ def teardown(instance):
   browser_instances[instance].quit()
 
 
-# Worker Thread
 def worker(instance):
+  # The Worker Thread for each webdriver instance
 
   # Begin by loggin into site if enabled
-  if authentication['enabled']:
+  if authentication['browser_authentication']['enabled']:
     authenticate(instance)
+    time.sleep(AUTH_SLEEP_INTERVAL)
   
   # Visit each page
   for endpoint in url_details['endpoints']:
     full_url = "%s://%s/" %(url_details['protocol'], url_details['url'])
     go_to_page(instance, "%s%s" %(full_url, endpoint))
+    time.sleep(PAGE_SLEEP_INTERVAL)
   
   # Clean up browsers after test
   teardown(instance)
@@ -56,6 +91,17 @@ def worker(instance):
 
 # Program Logic
 if __name__ == '__main__':
+
+  # How long to wait for an element to appear before throwing a TimeoutException
+  TIMEOUT = 5
+
+  # How long to sleep before requesting the next page (to wait for all AJAX elements to load)
+  # Alternatively, you can add a WebDriverWait until certain elements are present, but this is hard to generalize
+  PAGE_SLEEP_INTERVAL = 1
+
+  # How long to wait after authenticating (to wait for all AJAX elements to load)
+  AUTH_SLEEP_INTERVAL = 2
+
 
   # Import configured totals for each browser type
   with open('config/browser_totals.json', 'r') as tally_of_browsers:
@@ -76,10 +122,16 @@ if __name__ == '__main__':
   for browser_type in browsers:
     for x in range(0, browsers[browser_type]):
       
-      if browser_type == 'phantom':
+      if browser_type == 'headless':
+
+        # PhantomJS Capabilities for custom timeout length
         capabilities = webdriver.DesiredCapabilities.PHANTOMJS
         capabilities["phantomjs.page.settings.resourceTimeout"] = 1000
-        browser_instances.append(webdriver.PhantomJS(desired_capabilities=capabilities, service_args=['--ignore-ssl-errors=true', '--web-security=false']))
+
+        # Arguments to disable all SSL errors for unverified HTTPS connections
+        service_args = ['--ignore-ssl-errors=true', '--web-security=false']
+
+        browser_instances.append(webdriver.PhantomJS(desired_capabilities=capabilities, service_args=service_args))
         browser_instances[-1:][0].implicitly_wait(3)
       elif browser_type == 'firefox':
         browser_instances.append(webdriver.Firefox())
@@ -93,6 +145,8 @@ if __name__ == '__main__':
   print '\n'
 
   # Spawn one worker thread for each function
+  # Chrome/PhantomJS/Firefox browsers should be separate subprocesses, so there is likely
+  # To be no great advantage for swapping the threading and multiprocessing libraries
   threads = []
   i = 0
   for browser in browser_instances:
