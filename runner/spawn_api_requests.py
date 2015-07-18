fb5142fad51a61404a85b8b587fd6ae5a3e09a2e
Jason Willems <jason@willems.ca>
2015-07-18 18:30:48 -0400

    Better thread safety, better payload authentication, disabled more requests (urllib3) SSL warnings

--- runner/spawn_api_requests.py
+++ runner/spawn_api_requests.py
@@ -8,6 +8,8 @@ import signal
 import sys
 import argparse
 import json
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning
 
 
 ###############################################################################
@@ -18,15 +20,17 @@ import json
 ###############################################################################
 
 
-# Catch SIGTERM from User
-# If this fails, and processes are left running, execute:
-#   kill -9 `ps -ef | grep -i python | grep -i spawn_api_requests | head -n1 | awk '{ print $2 }'`
+
 def signal_term_handler(signal, frame):
+  # Catch SIGTERM from User
+  # If this fails, and processes are left running, execute:
+  #   kill -9 `ps -ef | grep -i python | grep -i spawn_api_requests | head -n1 | awk '{ print $2 }'`
   print("Script forced to aborted by user")
   sys.exit()
 
-# Print Colorized Status Codes (unix OSes only)
 def status(code):
+  # Print Colorized Status Codes (unix OSes only)
+
   # 2XX HTTP Response (Green)
   if str(code)[0] == "2":
     return "\033[92m" + str(code) + "\033[0m"
@@ -37,32 +41,43 @@ def status(code):
   else:
     return "\033[93m" + str(code) + "\033[0m"
 
+def safe_print(string, lock):
+  # Threadsafe version of print
+  # print() is threadsafe by default, however newlines are not
+  lock.acquire()
+  print(string)
+  lock.release()
+
 
 # Method to authenticate and send requests for each worker
-def send_http_request(results, times, pages, authentication, lock, timeout, number=1):
+def send_http_request(results, times, pages, authentication, timeout, lock, number=1):
 
   # Optional authentication step
   if authentication['api_authentication']['enabled']:
     try:
       print("\nAuthenticating thread number %s" %(number))
-      r = requests.request( 
-                            authentication['api_authentication']['request_type'],
-                            url="%s://%s/%s" %(url_details['protocol'], url_details['url'], authentication['api_authentication']['endpoint']),
-                            json=authentication['api_authentication']['payload_as_string'],
-                            verify=False
-                            )
+
+      request_type = authentication['api_authentication']['request_type']
+      login_url = "%s://%s/%s" %(url_details['protocol'], url_details['url'], authentication['api_authentication']['endpoint'])
+      header = {'Content-Type': authentication['api_authentication']['payload_format']}
+      payload = authentication['api_authentication']['payload_as_string']
+
+      r = requests.request(request_type, url=login_url, headers=header, data=payload, verify=False, allow_redirects=True)
+
     except Exception as e:
+      lock.acquire()
       print("Failed to send Authentication Request. Failure Response:")
       traceback.print_exc()
+      lock.relase()
       sys.exit()
 
     if r.status_code in [200, 201]:
       cookies = dict(r.cookies)
-      print("Authentication Succeeded\n\tSession Cookie: %s" % (dict(cookies)))
-      if not cookies.has_key('sessionId'):
-        print("\t\033[91mWarning:\033[0m Received 2XX status from server, but no Session Cookie was returned. You're probably NOT authenticated")
+      safe_print("Authentication Succeeded\n\tSession Cookie: %s" % (dict(cookies)), lock)
+      if sum(1 for _ in r.cookies) == 0:
+        safe_print("\t\033[91mWarning:\033[0m Received 2XX status from server, but no Session Cookie was readable. You're probably NOT authenticated", lock)
     else:
-      print("Authentication Failure:\n\tStatus: %s\n\tResponse: %s" %(status(r.status_code), r.text))
+      safe_print("Authentication Failure:\n\tStatus: %s\n\tResponse: %s" %(status(r.status_code), r.text), lock)
       sys.exit()
   else:
     cookies = {}
@@ -70,43 +85,49 @@ def send_http_request(results, times, pages, authentication, lock, timeout, numb
   # After authentication, traverse through each page
   try:
     for page in url_details['endpoints']:
-      lock.acquire()
+
+      current_url = "%s://%s/%s" %(url_details['protocol'], url_details['url'], page)
+      
       try:
         r = requests.request( 'get', 
-                              url="%s://%s/%s" %(url_details['protocol'], url_details['url'], page), 
+                              url=current_url, 
                               cookies=cookies, 
-                              timeout=(timeout['connect'], timeout['read']),
-                              verify=False
-                              )
+                              verify=False,
+                              allow_redirects=True,
+                              timeout=(timeout['connect'], timeout['read']))
+
         times.append(r.elapsed.microseconds)
         results["Valid Response"] += 1
       except requests.exceptions.ReadTimeout as e:
-        print("Request Thread %s:\n\t\033[91mRead Timeout!\033[0m No server response in %s seconds" %(number, timeout['read']))
+        safe_print("Request Thread %s:\n\t\033[91mRead Timeout!\033[0m No server response in %s seconds" %(number, timeout['read']), lock)
         results["Read Timeout"] += 1
         return
       except requests.exceptions.ConnectTimeout as e:
-        print("Request Thread %s:\n\t\033[91mConnect Timeout!\033[0m No server response in %s seconds" %(number, timeout['connect']))
+        lock.acquire()
+        safe_print("Request Thread %s:\n\t\033[91mConnect Timeout!\033[0m No server response in %s seconds" %(number, timeout['connect']), lock)
+        lock.release()
         results["Connect Timeout"] += 1
         return
       except requests.exceptions.ConnectionError as e:
-        print("Request Thread %s:\n\t\033[91mConnection Error!\033[0m %s" %(number, e))
+        safe_print("Request Thread %s:\n\t\033[91mConnection Error!\033[0m %s" %(number, e), lock)
         results["Connection Error"] += 1
         return
       except Exception as e:
-        print("Request Thread %s:\n\t\033[91mUnexpected Error!\033[0m %s" %(number, e))
+        safe_print("Request Thread %s:\n\t\033[91mUnexpected Error!\033[0m %s" %(number, e), lock)
         return
 
       if not r.status_code == 200:
-        print("Failure to get page:\n\tURL: %s\n\tStatus: %s\n\tResponse: %s" %(page, status(r.status_code), r.text))
+        safe_print("Failed to get page:\n\tURL: %s\n\tStatus: %s" %(current_url, status(r.status_code)), lock)
       else:
         if r.history:
           for redirect in r.history:
-            print("Request Thread %s:\n\tStatus: %s\n\tTime: %s\n\tRedirects:" %(number, status(r.status_code), float(r.elapsed.microseconds) / 1000000))
-            print("\t\t%s : %s" %(status(redirect.status_code), redirect.url))
-          print("\tFinal Destination:\n\t\t%s : %s" %(status(r.status_code), r.url))
+            safe_print("Request Thread %s:\n\tStatus: %s\n\tTime: %s\n\tRedirects:\n\t\t%s : %s" %( number, status(r.status_code), float(r.elapsed.microseconds) / 1000000,
+                                                                                                    status(redirect.status_code), redirect.url), 
+                                                                                                    lock)
+          safe_print("\tFinal Destination:\n\t\t%s : %s" %(status(r.status_code), r.url), lock)
         else:
-          print("Request Thread %s:\n\tURL: %s\n\tStatus: %s\n\tTime: %s" %(number, r.url, status(r.status_code), float(r.elapsed.microseconds) / 1000000))
-      lock.release()
+          safe_print("Request Thread %s:\n\tURL: %s\n\tStatus: %s\n\tTime: %s" %(number, r.url, status(r.status_code), float(r.elapsed.microseconds) / 1000000), lock)
+
   except KeyboardInterrupt:
     sys.exit()
 
@@ -114,8 +135,13 @@ def send_http_request(results, times, pages, authentication, lock, timeout, numb
 
 if __name__ == "__main__":
 
-  # Disable SSL warnings
-  requests.packages.urllib3.disable_warnings()
+  # Disable all SSL warnings
+  try:
+    requests.packages.urllib3.disable_warnings()
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+  except:
+    pass
 
   # Catch Sigterm from User
   signal.signal(signal.SIGTERM, signal_term_handler)
@@ -170,8 +196,6 @@ if __name__ == "__main__":
   for subprocess in subprocesses:
       subprocess.join()
 
-
-
   # Calculate average response time
   # Average Time in seconds
   avg_time = "N/A"
@@ -179,6 +203,7 @@ if __name__ == "__main__":
     avg_time = float(sum(times)/len(times))/1000000
 
   # Print Results to Console
+  # JW: TODO clean these up so they don't throw exceptions for empty data setsg
   print("\nAll Requests Sent:")
   print("\tValid Response: %s" %(results["Valid Response"]))
   print("\tConnection Error: %s" %(results["Connection Error"]))
